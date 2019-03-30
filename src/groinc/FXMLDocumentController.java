816a13ff11bd1eeb10c127682fd8f94a8a1886e4
shehabkhalid <shehabkhalid1999@gmail.com>
2019-03-30 13:21:26 +0200

    Animations Test
    Just a Scale Animation Added to the sample Test

--- src/groinc/FXMLDocumentController.java
+++ src/groinc/FXMLDocumentController.java
@@ -11,6 +11,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.*;
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
+import javafx.geometry.Bounds;
+import javafx.util.Duration;
 
 /**
  *
@@ -27,7 +32,16 @@ public class FXMLDocumentController implements Initializable
     @FXML
     private  void testit(ActionEvent event)
       {
-        label2.setText("Welcome To GroInc !");
+          ScaleTransition aScaleTransition = new ScaleTransition();
+          aScaleTransition.setDuration(Duration.millis(500));
+          aScaleTransition.setNode(label2);
+          label2.setText("Welcome To GroInc !");
+          label2.setScaleX(0);
+          label2.setScaleY(0);
+          aScaleTransition.setByX(1);
+          aScaleTransition.setByY(1);
+          aScaleTransition.play();
+       
       }
     
     @Override
